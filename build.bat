131dde96144e5d3c141fa0ae1ccd752b2b122834
TomShaw333 <tshaw1191@outlook.com>
2025-04-26 16:09:19 -0400

    script patch

--- build.bat
+++ build.bat
@@ -3,11 +3,11 @@ cd snake-game
 
 if not exist node_modules (
   echo Installing dependencies...
-  npm install
+  call npm install
 )
 
 echo Building project...
-npm run build
+call npm run build
 
 echo Starting server in a new window...
 start "Next.js Server" cmd /k "npm run start"
